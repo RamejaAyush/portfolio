a02fbe2569b1b7d74b91d12b8187a0a25d5c9f20
Ayush Rameja <ayushrameja@Ayushs-MacBook-Pro.local>
2024-11-16 11:59:41 +0530

    Latest vite init

--- src/App.tsx
+++ src/App.tsx
@@ -1,98 +1,35 @@
-import Lenis from "lenis";
-import "./styles/app.scss";
-import { Suspense, lazy } from "react";
-import { useEffect, useRef } from "react";
-import { useAppStore } from "./store/store";
-import { Navigate, Route, Routes, useLocation } from "react-router-dom";
-
-const Nav = lazy(() => import("./components/Nav"));
-const Home = lazy(() => import("./components/Home"));
-const Blogs = lazy(() => import("./components/Blogs"));
-const Resume = lazy(() => import("./components/Resume"));
-const Loading = lazy(() => import("./components/Loading"));
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  const rafId = useRef<number | null>(null);
-  const lenisRef = useRef<Lenis | null>(null);
-  const previousPathRef = useRef<string | null>(null);
-
-  const isLoading = useAppStore((state) => state.isLoading);
-  const setNavClass = useAppStore((state) => state.setNavClass);
-  const setIsLoading = useAppStore((state) => state.setIsLoading);
-  const setCurrentRoute = useAppStore((state) => state.setCurrentRoute);
-  const setShowExternal = useAppStore((state) => state.setShowExternal);
-
-  const location = useLocation();
-
-  useEffect(() => {
-    const initializeAnimation = () => {
-      lenisRef.current = new Lenis({
-        lerp: 0.1,
-      });
-
-      const raf = (time: number) => {
-        if (lenisRef.current) {
-          lenisRef.current.raf(time);
-        }
-        rafId.current = requestAnimationFrame(raf);
-      };
-
-      rafId.current = requestAnimationFrame(raf);
-    };
-
-    window.addEventListener("load", initializeAnimation);
-
-    return () => {
-      if (rafId.current) {
-        cancelAnimationFrame(rafId.current);
-      }
-      window.removeEventListener("load", initializeAnimation);
-    };
-  }, []);
-
-  useEffect(() => {
-    const pathName = location.pathname;
-
-    if (previousPathRef.current !== pathName) {
-      let route = "Home";
-      if (pathName.includes("/blogs")) {
-        route = "Blogs";
-      } else if (pathName.includes("/resume")) {
-        route = "Resume";
-      }
-
-      setIsLoading(true);
-      setNavClass("grey");
-      setCurrentRoute(route);
-      setShowExternal(false);
-
-      const timeout = setTimeout(() => {
-        setIsLoading(false);
-      }, 1500);
-
-      previousPathRef.current = pathName;
-
-      return () => clearTimeout(timeout);
-    }
-  }, [location, setCurrentRoute, setIsLoading, setNavClass, setShowExternal]);
-
-  if (isLoading) {
-    return <Loading />;
-  }
+  const [count, setCount] = useState(0)
 
   return (
-    <div className="app">
-      <Suspense fallback={<Loading />}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/blogs" element={<Blogs />} />
-          <Route path="/Resume" element={<Resume />} />
-          <Route path="*" element={<Navigate replace to={"/"} />} />
-        </Routes>
-        <Nav />
-      </Suspense>
-    </div>
-  );
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import "./index.scss";
-import App from "./App.tsx";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-createRoot(document.getElementById("root")!).render(
-  <Router>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
     <App />
-  </Router>
-);
+  </StrictMode>,
+)
