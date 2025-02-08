3bdcd3ead60685eaf874aa4f44d1a112b8166019
Ayush Rameja <ayushrameja@Ayushs-MacBook-Pro.local>
2025-02-08 18:52:33 +0530

    Removed entire codebase because it sucks, thanks past me :)

--- README.md
+++
@@ -1,5 +0,0 @@
-# Ayush Rameja's 2024 Portfolio
-
-Please checkout https://www.ayushrameja.com
-
-© Ayush Rameja
--- src/models/IProject.ts
+++
@@ -1,9 +0,0 @@
-export interface IProject {
-  id: number;
-  name: string;
-  role: string;
-  client: string;
-  skills: string[];
-  points: string[];
-  link: string;
-}
